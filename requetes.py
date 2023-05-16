a2a215162323c7872642252a88a5a0a4fcb84100
Arnaud TRAVERT <atravert@users.noreply.github.com>
2023-05-16 14:21:04 +0200

    Add files via upload

---
+++ requetes.py
@@ -0,0 +1,55 @@
+
+ETUDIANTX = (
+    '"étudiant" OR "étudiants"',
+    '"étudiante" OR "étudiantes"',
+    '"étudiant.e" OR "étudiant.e.s" OR "étudiant.es"',
+    )
+
+DOCTORANTX = (
+    '"doctorant" OR "doctorants"',
+    '"doctorante" OR "doctorantes"',
+    '"doctorant.e" OR "doctorant.e.s" OR "doctorant.es"',  
+    )
+
+CHERCHEUX = (
+    '"chercheur" OR "chercheurs"',  
+    '"chercheuse" OR "chercheure" OR "chercheuses" OR "chercheures"',
+    '"chercheur.euse" OR "chercheur.euse.s" OR "chercheur.euses" OR "chercheur.e" OR "chercheur.e.s" OR "chercheur.es"'
+    )
+
+ENSEIGNANTX = (
+    '"enseignant" OR "enseignants"',
+    '"enseignante" OR "enseignantes"',
+    '"enseignant.e" OR "enseignant.e.s" OR "enseignant.es"'
+    )
+
+PROFESSEURX = (
+    '"professeur" OR "professeurs"',
+    '"professeure" OR "professeures"',
+    '"professeur.e" OR "professeur.e.s" OR "professeur.es"'
+    )
+
+MCFX = (
+    '"Maître de conférences" OR "Maîtres de conférences"',
+    '"Maîtresse de conférences" OR "Maîtresses de conférences"',
+    '"Maître.esse de conférences" OR "Maître.esse.s de conférences" OR "Maître.esses de conférences"'
+    )
+
+MAITREX = (
+    '"Maître" OR "Maîtres"',
+    '"Maîtresse" OR "Maîtresses"',
+    '"Maître.esse" OR "Maître.esse.s" OR "Maître.esses"'
+    )
+
+DIRECTEURX = (
+    '"directeur" OR "directeurs"',
+    '"directrice" OR "directrices"', 
+    '"directeur.ice" OR "directeur.ice.s"OR "directeur.ices"' 
+    )
+
+DRX = (
+    '"directeur de recherche" OR "directeurs de recherche" OR directeur de recherches" OR "directeurs de recherches" ',
+    '"directrice de recherches" OR "directrices de recherches" OR "directrice de recherches" OR "directrices de recherches" OR "directrice de recherches" OR "directrices de recherches"',
+    '"directeur.ice de recherche" OR "directeur.ice.s de recherche" OR "directeur.ices de recherche" OR "directeur.ice de recherches" OR "directeur.ice.s de recherches" OR "directeur.ices de recherches"'
+    )
+
