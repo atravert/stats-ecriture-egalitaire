31f2334a42b2bf14ff588a7068a7da3f2f7cf396
Arnaud TRAVERT <atravert@users.noreply.github.com>
2023-05-16 15:10:09 +0200

    Add files via upload

--- analyse.py
+++ analyse.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 # Load data
 df = pd.read_excel(Path("results_230515_raw_fr.xlsx"))
 
+# %%
 # plot 
 from matplotlib.ticker import FuncFormatter
 plt.style.use('ggplot')
@@ -20,7 +21,7 @@ def thousands(x, pos):
     return '%1.fk' % (x * 1e-3)
 formatter = FuncFormatter(thousands)
 
-def plot_bar(loc, factor=1.):
+def plot_bar(loc, factor=1., alt_title=None):
     '''barplot
     
     loc: str or int
@@ -33,7 +34,11 @@ def plot_bar(loc, factor=1.):
     df_ = df.iloc[:,i:i+3]
     df_.columns = ['Masculin', 'Féminin', 'Contracté']
     ax = (df_.sum(axis=0) * factor).plot.bar(color=colors, rot=0)
-    ax.set_title(df.columns[i], y=0.87)
+    if alt_title is not None:
+        title = alt_title
+    else: 
+        title = df.columns[i]
+    ax.set_title(title, y=0.87)
     ax.yaxis.set_major_formatter(formatter)
     ax.set_yticks([2e5, 4e5, 6e5, 8e5])
     ax.set_ylim([0, 0.8e6])
@@ -42,17 +47,17 @@ mm = 1/25.4
 fig, axes = plt.subplots(nrows=3, ncols=2, tight_layout=True, figsize=(183*mm, 220*mm), dpi=80)
 
 plt.sca(axes[0,0])
-plot_bar(0)
-# plt.sca(axes[0,1])
-# plot_bar(6)
-# plt.sca(axes[1,0])
-# plot_bar(9)
-# plt.sca(axes[1,1])
-# plot_bar(12)
-# plt.sca(axes[2,0])
-# plot_bar(15, factor=0.5)
-# plt.sca(axes[2,1])
-# plot_bar(18,)
+plot_bar('enseignant', alt_title='Enseignant*')
+plt.sca(axes[0,1])
+plot_bar('chercheur', alt_title='Chercheu*')
+plt.sca(axes[1,0])
+plot_bar('professeur', alt_title='Professeur*')
+plt.sca(axes[1,1])
+plot_bar('Maître de conférences', factor=2, alt_title='Maître* de conférences (*2)')
+plt.sca(axes[2,0])
+plot_bar('étudiant', factor=0.5, alt_title='Etudiant*')
+plt.sca(axes[2,1])
+plot_bar('doctorant', alt_title='Doctorant*')
 
 for ax in axes.flat:
     ax.label_outer()
@@ -64,14 +69,14 @@ def invisibilisation(i):
     return 100 - (df.iloc[:,i] - df.iloc[:,i+1]) / df.iloc[:,i] * 100
 
 sum =  (df.sum(axis=1))
-size = sum/max(sum) * 150
+size = sum/max(sum) * 10
 
-i_mcf = invisibilisation(3)
-i_ch = invisibilisation(6)
-i_ens = invisibilisation(9)
-i_pr= invisibilisation(12)
-i_etu= invisibilisation(15)
-i_doc= invisibilisation(18)
+i_mcf = invisibilisation(5)
+i_ch = invisibilisation(8)
+i_ens = invisibilisation(11)
+i_pr= invisibilisation(14)
+i_etu= invisibilisation(17)
+i_doc= invisibilisation(20)
 
 fig, axes = plt.subplots(nrows=2, ncols=2, tight_layout=True, figsize=(183*mm, 183*mm), dpi=80)
 plt.sca(axes[0,0])
